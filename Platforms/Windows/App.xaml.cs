b2dbce41f446e65968744fb912e884ecc2abfbdf
ikirsul'ev <ikirsul'ev@DJ-MB-K205-05>
2024-11-28 15:23:28 +0300

    Добавьте файлы проекта.

---
+++ Platforms/Windows/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.UI.Xaml;
+
+
+namespace Notes.WinUI
+{
+    
+    public partial class App : MauiWinUIApplication
+    {
+        
+        public App()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+
+}
